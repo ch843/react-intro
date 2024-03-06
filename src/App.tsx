c1e3eb1430316da5934c662bbd2c7b29bdf49a09
ch843 <cassidy.hardisty@gmail.com>
2024-03-06 09:53:44 -0700

    Finished mission 9

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,66 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import teamData from './CollegeBasketballTeams.json'
 
-function App() {
+interface TeamInfo {
+  tid: number;
+  cid: number;
+  did: number;
+  school: string;
+  name: string;
+  abbrev: string;
+  pop: number;
+  city: string;
+  state: string;
+  latitude: number;
+  longitude: number;
+}
+
+const teams: TeamInfo[] = teamData.teams as TeamInfo[];
+
+function Welcome() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="team">
+      <h1>NCAA Men's Basketball Teams</h1>
+      <p>Prepare for March Madness with this list of all NCAA teams!</p>
+      <br/>
     </div>
   );
+}
+
+  class Team extends React.Component<TeamInfo>
+  {
+    render()
+    {
+      let oneTeam : TeamInfo = this.props;
+      return (
+        <div className="team">
+          <h2>{oneTeam.school}</h2>
+          <p>{oneTeam.name}</p>
+          <p>{oneTeam.city}, {oneTeam.state}</p>
+          <br/>
+        </div>
+      );
+    }
+  }
+function TeamList() {
+  return (
+    <>
+      {teams.map((team) => (
+        <Team key={team.name} {...team} />
+        ))}
+    </>
+  );
+}
+
+function App() {
+
+  return (
+    <>
+      <Welcome />
+      <TeamList />
+    </>
+  )
 }
 
 export default App;
